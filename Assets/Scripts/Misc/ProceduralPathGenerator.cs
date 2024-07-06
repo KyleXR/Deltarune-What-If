03b8a734b0ac0a4d145e8b562c1c38a403b0521b
LesserFlame <94986192+LesserFlame@users.noreply.github.com>
2024-07-05 20:32:53 -0600

    Path Spline Goober Beggingnging

--- Assets/Scripts/Misc/ProceduralPathGenerator.cs
+++ Assets/Scripts/Misc/ProceduralPathGenerator.cs
@@ -4,7 +4,7 @@ namespace PathCreation.Examples {
     // Example of creating a path at runtime from a set of points.
 
     [RequireComponent(typeof(PathCreator))]
-    public class GeneratePathExampleTest : MonoBehaviour {
+    public class ProceduralPathGenerator : MonoBehaviour {
 
         public bool closedLoop = true;
         public Transform[] waypoints;
@@ -13,8 +13,11 @@ namespace PathCreation.Examples {
             if (waypoints.Length > 0) {
                 // Create a new bezier path from the waypoints.
                 BezierPath bezierPath = new BezierPath (waypoints, closedLoop, PathSpace.xyz);
-                GetComponent<PathCreator> ().bezierPath = bezierPath;
+                var oldBezier = GetComponent<PathCreator>().bezierPath;
+                float globalNormals = oldBezier.GlobalNormalsAngle;
+                GetComponent<PathCreator>().bezierPath = bezierPath;
+                bezierPath.GlobalNormalsAngle = globalNormals;
             }
         }
     }
-}
+}   
