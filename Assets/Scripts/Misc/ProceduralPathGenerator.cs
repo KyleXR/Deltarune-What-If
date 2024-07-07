7ebcb2bb356d4b5e64bcce91cfa9bcddbd31efc7
LesserFlame <94986192+LesserFlame@users.noreply.github.com>
2024-07-06 20:38:54 -0600

    stuffs

--- Assets/Scripts/Misc/ProceduralPathGenerator.cs
+++ Assets/Scripts/Misc/ProceduralPathGenerator.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace PathCreation.Examples {
     // Example of creating a path at runtime from a set of points.
@@ -16,10 +18,12 @@ namespace PathCreation.Examples {
 
         public void GeneratePath()
         {
+            Queue<Transform> test = new Queue<Transform>(waypoints);
+            
             if (waypoints.Length > 0)
             {
                 // Create a new bezier path from the waypoints.
-                BezierPath bezierPath = new BezierPath(waypoints, closedLoop, PathSpace.xyz);
+                BezierPath bezierPath = new BezierPath(test, closedLoop, PathSpace.xyz);
                 var oldBezier = GetComponent<PathCreator>().bezierPath;
                 float globalNormals = oldBezier.GlobalNormalsAngle;
                 GetComponent<PathCreator>().bezierPath = bezierPath;
