b83602c64757f2e068b71e7485453bafd366570d
Kyle Reed <kyreed@student.neumont.edu>
2024-07-15 10:16:42 -0600

    Track Bending

--- Assets/Scripts/Misc/ProceduralPathGenerator.cs
+++ Assets/Scripts/Misc/ProceduralPathGenerator.cs
@@ -5,10 +5,12 @@ namespace PathCreation.Examples {
     // Example of creating a path at runtime from a set of points.
 
     [RequireComponent(typeof(PathCreator))]
-    public class ProceduralPathGenerator : MonoBehaviour {
+    public class ProceduralPathGenerator : MonoBehaviour 
+    {
 
         private PathCreator creator;
         public bool closedLoop = true;
+        public bool hasTurned = false;
         public float updateTimer = 0.0f;
         public GameObject sphere;
         public PathFollower cart;
@@ -44,12 +46,29 @@ namespace PathCreation.Examples {
             }
         }
 
+        public void GenerateCurvedPath()
+        {
+            //float x = 0f; // replace with your desired rotation value
+            //float y = 0f; // replace with your desired rotation value
+            //float z = 0f; // replace with your desired rotation value
+
+            // Create the Quaternion for the desired rotation
+            //Quaternion rotation = Quaternion.Euler(x, y, z);
+
+            var sphereOne = Instantiate(sphere, new Vector3(5, 0, spherePosition), Quaternion.identity); //rotation);
+            waypoints.Add(sphereOne.transform);
+            hasTurned = !hasTurned;
+            GeneratePath();
+            creator.TriggerPathUpdate();
+
+        }
+
         public void GenerateWaypoint()
         {
-            var newSphere = Instantiate(sphere, new Vector3(0, 0, spherePosition), Quaternion.identity);
-            waypoints.Add(newSphere.transform);
-            spherePosition += 5;
-            updateTimer = 0.0f;
+            float offset = (hasTurned ? 10 : 0);
+            
+            if(!hasTurned) spherePosition += 50;
+            else spherePosition -= 50;
 
 
             if (waypoints.Count > 4)
@@ -59,6 +78,15 @@ namespace PathCreation.Examples {
                 Destroy(lastPosition.gameObject);
             }
 
+            if (spherePosition == 1000f || spherePosition == -1000f)
+            {
+                GenerateCurvedPath();
+            }
+            else
+            {
+                var newSphere = Instantiate(sphere, new Vector3(offset, 0, spherePosition), Quaternion.identity);
+                waypoints.Add(newSphere.transform);
+            }
             GeneratePath();
             creator.TriggerPathUpdate();
         }
