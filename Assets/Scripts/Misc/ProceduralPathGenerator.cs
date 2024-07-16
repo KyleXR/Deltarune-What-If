5c872bb27495e61463615ce985c36e3102452a67
Kyle Reed <kyreed@student.neumont.edu>
2024-07-16 10:55:02 -0600

    Tracks implemented

--- Assets/Scripts/Misc/ProceduralPathGenerator.cs
+++ Assets/Scripts/Misc/ProceduralPathGenerator.cs
@@ -14,7 +14,7 @@ namespace PathCreation.Examples {
         public float updateTimer = 0.0f;
         public GameObject sphere;
         public PathFollower cart;
-        public int spherePosition = 0;
+        public float spherePosition = 0;
         public List<Transform> waypoints;
 
         void Start () 
@@ -56,6 +56,7 @@ namespace PathCreation.Examples {
             //Quaternion rotation = Quaternion.Euler(x, y, z);
 
             var sphereOne = Instantiate(sphere, new Vector3(5, 0, spherePosition), Quaternion.identity); //rotation);
+            sphereOne.transform.rotation = Quaternion.LookRotation(new Vector3(0,90,0), new Vector3(0,0,0));
             waypoints.Add(sphereOne.transform);
             hasTurned = !hasTurned;
             GeneratePath();
@@ -67,8 +68,8 @@ namespace PathCreation.Examples {
         {
             float offset = (hasTurned ? 10 : 0);
             
-            if(!hasTurned) spherePosition += 50;
-            else spherePosition -= 50;
+            if(!hasTurned) spherePosition += 34.5f;
+            else spherePosition -= 34.5f;
 
 
             if (waypoints.Count > 4)
@@ -78,13 +79,14 @@ namespace PathCreation.Examples {
                 Destroy(lastPosition.gameObject);
             }
 
-            if (spherePosition == 1000f || spherePosition == -1000f)
+            if (spherePosition == 1000.5f || spherePosition == -1000.5f)
             {
                 GenerateCurvedPath();
             }
             else
             {
                 var newSphere = Instantiate(sphere, new Vector3(offset, 0, spherePosition), Quaternion.identity);
+                newSphere.transform.rotation = Quaternion.LookRotation(new Vector3(0, 90, 0), new Vector3(0, 0, 0));
                 waypoints.Add(newSphere.transform);
             }
             GeneratePath();
